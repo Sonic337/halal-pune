49a111ad111a522b322056e769e915176be2e249
Sonic <sonicthesaga1@gmail.com>
2026-06-20 16:10:24 +0530

    feat: add Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -87,6 +88,7 @@ export default function RootLayout({
           <a href="/forms">Suggest a Restaurant</a>
         </nav>
         {children}
+        <GoogleAnalytics gaId="G-6ZHV6TD3Q1" />
       </body>
     </html>
   );
